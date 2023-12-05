bf27e4633ff7f4e5194c0a753038bafd54f86ccd
Brandon Navra <brandon.navra@gmail.com>
2023-12-05 16:32:08 +1100

    Update configuration

--- setup.py
+++ setup.py
@@ -5,23 +5,21 @@ import sys
 
 from setuptools import find_packages, setup
 
-from __version__ import __version__
-
 
 # Allow editable install into user site directory.
 site.ENABLE_USER_SITE = "--user" in sys.argv[1:]
 
 setup(
     name="IPython Copilot Completer",
-    version=__version__,
+    version="0.0.8",
     description="Use GitHub Copilot in IPython to autocomplete your code",
-    long_description=open("README.md").read(),  # noqa: SIM115
+    long_description=open("README.md").read(),
     author="Brandon Navra",
     author_email="brandon.navra@gmail.com",
     packages=find_packages(),
     license="MIT",
     install_requires=[
-        "IPython>=8.9.0",
+        "IPython>=8.18.0",
         "requests",
     ],
 )
